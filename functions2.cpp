4e7551e7bb4c724168f3cd44ae9967a625c53560
Stepan Andreichev <85014298+StepWolf45@users.noreply.github.com>
2023-02-06 13:27:45 +0300

    Update functions2.cpp

--- functions2.cpp
+++ functions2.cpp
@@ -6,25 +6,31 @@ using namespace std;
 
 void itc_rev_list(vector<int> &mass) {
   int temp;
-  for (int i = 0; i <= mass.size() / 2; ++i) {
-    temp = mass[i];
-    mass[i] = mass[mass.size() - 1 - i];
-    mass[mass.size() - 1 - i] = temp;
+  if(mass.size()>0){
+    for (int i = 0; i <= mass.size() / 2; ++i) {
+      temp = mass[i];
+      mass[i] = mass[mass.size() - 1 - i];
+      mass[mass.size() - 1 - i] = temp;
+    }
   }
 }
 void itc_rev_par_list(vector<int> &mass) {
   int temp;
-  for (int i = 0; i < mass.size() - 1; i += 2) {
-    temp = mass[i];
-    mass[i] = mass[i + 1];
-    mass[i + 1] = temp;
-  }
+  if (mass.size() > 0){
+    for (int i = 0; i < mass.size() - 1; i += 2) {
+      temp = mass[i];
+      mass[i] = mass[i + 1];
+      mass[i + 1] = temp;
+    }
+   }
 }
 void itc_rshift_list(vector<int> &mass) {
   int temp;
-  int null = mass[mass.size() - 1];
-  for (int i = mass.size() - 1; i > 0; --i) {
-    mass[i] = mass[i - 1];
+  if (mass.size() > 0){
+    int null = mass[mass.size() - 1];
+    for (int i = mass.size() - 1; i > 0; --i) {
+      mass[i] = mass[i - 1];
+    }
+    mass[0] = null;
   }
-  mass[0] = null;
 }
