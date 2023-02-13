fbd3547249bbad9f25dddd1b0648948db7cac899
Stepan Andreichev <85014298+StepWolf45@users.noreply.github.com>
2023-02-13 22:22:56 +0300

    Update functions2.cpp

--- functions2.cpp
+++ functions2.cpp
@@ -34,22 +34,27 @@ void itc_rshift_list(vector<int> &mass) {
     mass[0] = null;
   }
 }
+
+void itc_lshift_list(vector <int> &mass){
+  if (mass.size() > 0){
+    int tmp = mass[0];
+    for(int i = 1; i < mass.size(); ++i){
+      mass[i - 1] = mass[i];
+      mass[mass.size() - 1] = tmp;
+    }
+  }
+}
+
 void itc_super_shift_list(vector <int> &mass, int n){
   if (mass.size() > 0){
     if(n > 0){
-      for(int i = 0; i < (n % mass.size()); ++i){
-          itc_rshift_list(mass);
+      for(int i = 0; i < (n % mass.size()); ++i) {
+        itc_rshift_list(mass);
       }
     }else if(n < 0){
       n = -n;
-      for(int i = 0; i < (n % mass.size()); ++i){ 
-        if (mass.size() > 0){
-          int zero = mass[0];
-          for(int i = 1; i < mass.size(); ++i){
-            mass[i - 1] = mass[i];
-          }
-          mass[mass.size() - 1] = zero;
-        }
+      for(int i = 0; i < (n % mass.size()); ++i){
+        itc_lshift_list(mass);
       }
     } 
   } 
